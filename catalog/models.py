54cb3f052d1e9c2ce6dd489451b4d12b9d72b06d
Danila Kharchenko <spook9400@mail.ru>
2023-10-22 13:05:43 +0300

    add homework 20.1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(verbose_name='Изображение', upload_to='img/', **NULLABLE)
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='Цена', **NULLABLE)
+    data_created = models.DateTimeField(verbose_name='Дата последнего изменения', auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
